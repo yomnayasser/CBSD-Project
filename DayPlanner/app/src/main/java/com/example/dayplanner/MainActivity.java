662f9067947f10e0470e73d87bcedb97174cffe8
Abdulrahman Ghonaim <abdoghonaim@gmail.com>
2021-05-22 22:25:54 +0200

    Create New Reminder

--- DayPlanner/app/src/main/java/com/example/dayplanner/MainActivity.java
+++ DayPlanner/app/src/main/java/com/example/dayplanner/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.dayplanner;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button remindersBtn = (Button)findViewById(R.id.remindersBtn);
+
+        remindersBtn.setOnClickListener(v -> {
+            Intent i = new Intent(MainActivity.this, RemindersActivity.class);
+            startActivity(i);
+        });
     }
 }
